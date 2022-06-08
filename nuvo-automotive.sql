9e2411bc473f44613b6659a0bf9e6bf08dea32a2
Danko Sanchez <lhdyg2013@gmail.com>
2022-06-08 01:47:06 -0400

    nuvo-automotive.sql

--- nuvo-automotive.sql
+++ nuvo-automotive.sql
@@ -1,33 +1,27 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
+-- MySQL dump 10.13  Distrib 5.7.33, for Win64 (x86_64)
 --
--- Servidor: localhost:3306
--- Tiempo de generación: 07-06-2022 a las 04:13:48
--- Versión del servidor: 5.7.33
--- Versión de PHP: 8.1.6
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: nuvo-automotive
+-- ------------------------------------------------------
+-- Server version	5.7.33
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Base de datos: `nuvo-automotive`
+-- Table structure for table `client`
 --
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `client`
---
-
-
+DROP TABLE IF EXISTS `client`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `client` (
   `RUT` varchar(255) NOT NULL,
   `FIRSTNAME` varchar(255) DEFAULT NULL,
@@ -37,49 +31,58 @@ CREATE TABLE `client` (
   `EMAIL` varchar(255) DEFAULT NULL,
   `ADDRESS` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish2_ci NOT NULL,
   `COMMUNE` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish2_ci NOT NULL,
-  `CREATE_AT` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `UPDATED_AT` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `CREATED_AT` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `UPDATED_AT` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`RUT`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Volcado de datos para la tabla `client`
+-- Dumping data for table `client`
 --
 
-INSERT INTO `client` (`RUT`, `FIRSTNAME`, `LASTNAME`, `SECOND_LASTNAME`, `PHONE`, `EMAIL`, `ADDRESS`, `COMMUNE`, `CREATE_AT`, `UPDATED_AT`) VALUES
-('19.432.142-4', 'Jose', 'Perez', 'Diaz', '+56942395888', 'jdiaz@gmail.com', 'Av Marathon #198', 'Macul', '2022-06-07 03:08:41', '2022-06-07 03:08:41'),
-('20.049.429-6', 'Danko', 'Sanchez', 'Arancibia', '+56958608563', 'danko@gmail.com', 'Av Las Condes #1213', 'Las Condes', '2022-06-07 02:15:04', '2022-06-07 02:39:45'),
-('21.546.455-3', 'Andres', 'Correa', 'Santana', '+56980023244', 'acorrea@gmail.com', 'San jose de la estrella #21199', 'La Florida', '2022-06-07 03:08:41', '2022-06-07 03:08:41');
-
--- --------------------------------------------------------
+LOCK TABLES `client` WRITE;
+/*!40000 ALTER TABLE `client` DISABLE KEYS */;
+INSERT INTO `client` VALUES ('19.432.142-4','Jose','Perez','Diaz','+56942395888','jdiaz@gmail.com','Av Marathon #198','Macul','2022-06-07 03:08:41','2022-06-07 03:08:41'),('20.049.429-6','Danko','Sanchez','Arancibia','+56958608563','danko@gmail.com','Av Las Condes #1213','Las Condes','2022-06-07 02:15:04','2022-06-07 02:39:45'),('21.546.455-3','Andres','Correa','Santana','+56980023244','acorrea@gmail.com','San jose de la estrella #21199','La Florida','2022-06-07 03:08:41','2022-06-07 03:08:41');
+/*!40000 ALTER TABLE `client` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Estructura de tabla para la tabla `mechanic`
+-- Table structure for table `mechanic`
 --
 
+DROP TABLE IF EXISTS `mechanic`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `mechanic` (
   `RUT` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish2_ci NOT NULL,
   `FIRSTNAME` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish2_ci NOT NULL,
   `LASTNAME` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish2_ci NOT NULL,
   `EMAIL` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish2_ci DEFAULT NULL,
   `PHONE` varchar(50) NOT NULL,
-  `CREATE_AT` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `UPDATED_AT` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `CREATED_AT` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `UPDATED_AT` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`RUT`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Volcado de datos para la tabla `mechanic`
+-- Dumping data for table `mechanic`
 --
 
-INSERT INTO `mechanic` (`RUT`, `FIRSTNAME`, `LASTNAME`, `EMAIL`, `PHONE`, `CREATE_AT`, `UPDATED_AT`) VALUES
-('15.523.523-K', 'Pablo', 'Castro', 'Soto', '+56942382289', '2022-06-07 03:15:29', '2022-06-07 03:15:29'),
-('17.091.242.6', 'Matias', 'Zuna', 'Taste', '+56954290998', '2022-06-07 03:15:29', '2022-06-07 03:15:29');
-
--- --------------------------------------------------------
+LOCK TABLES `mechanic` WRITE;
+/*!40000 ALTER TABLE `mechanic` DISABLE KEYS */;
+INSERT INTO `mechanic` VALUES ('15.523.523-K','Pablo','Castro','pcastro@gmail.cl','+56942382289','2022-06-07 03:15:29','2022-06-07 03:15:29'),('17.091.242.6','Matias','Zuna','mzuna@gmail.cl','+56954290998','2022-06-07 03:15:29','2022-06-07 03:15:29');
+/*!40000 ALTER TABLE `mechanic` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Estructura de tabla para la tabla `user`
+-- Table structure for table `user`
 --
 
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user` (
   `ID` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish2_ci NOT NULL,
   `ROLE` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish2_ci DEFAULT 'user',
@@ -88,23 +91,30 @@ CREATE TABLE `user` (
   `LASTNAME` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish2_ci DEFAULT NULL,
   `EMAIL` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish2_ci NOT NULL,
   `PASSWORD` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish2_ci NOT NULL,
-  `CREATE_AT` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `UPDATED_AT` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  `CREATED_AT` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `UPDATED_AT` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`ID`),
+  CONSTRAINT `IDWO` FOREIGN KEY (`ID`) REFERENCES `workorder` (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Volcado de datos para la tabla `user`
+-- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`ID`, `ROLE`, `RUT`, `FIRSTNAME`, `LASTNAME`, `EMAIL`, `PASSWORD`, `CREATE_AT`, `UPDATED_AT`) VALUES
-('1', 'admin', '123', 'Danko', 'Sanchez', 'admin@admin.cl', '$2y$10$KLrkP7cop3gXm6ELwCtg7u1Jd7V3pEHlJ64yLIbYCze0pdiPGbdMm', '2022-06-06 01:29:29', '2022-06-06 01:31:52');
-
--- --------------------------------------------------------
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES ('1','admin','123','Danko','Sanchez','admin@admin.cl','$2y$10$KLrkP7cop3gXm6ELwCtg7u1Jd7V3pEHlJ64yLIbYCze0pdiPGbdMm','2022-06-06 01:29:29','2022-06-06 01:31:52');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Estructura de tabla para la tabla `vehicle`
+-- Table structure for table `vehicle`
 --
 
+DROP TABLE IF EXISTS `vehicle`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `vehicle` (
   `PATENT` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish2_ci NOT NULL,
   `BRAND` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish2_ci DEFAULT NULL,
@@ -115,76 +125,53 @@ CREATE TABLE `vehicle` (
   `COLOR` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish2_ci DEFAULT NULL,
   `CHASSIS_NUMBER` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish2_ci DEFAULT NULL,
   `MILEAGE` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish2_ci DEFAULT NULL,
-  `VEHICLE_TYPE` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish2_ci DEFAULT NULL
+  `VEHICLE_TYPE` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish2_ci DEFAULT NULL,
+  `CREATED_AT` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `UPDATED_AT` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`PATENT`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Estructura de tabla para la tabla `workorder`
+-- Dumping data for table `vehicle`
 --
 
+LOCK TABLES `vehicle` WRITE;
+/*!40000 ALTER TABLE `vehicle` DISABLE KEYS */;
+INSERT INTO `vehicle` VALUES ('BKJS-42','Audi','A4','2022','Bencina','Manual','Blanco','#A48298374','8.982','Sedan','2022-06-08 00:06:20','2022-06-08 00:06:20'),('MLBV-56','Chevrolet','Captiva','2012','Petróleo','Manual','Plateado','#A92908422','135.000','SUV','2022-06-08 00:06:20','2022-06-08 00:06:20'),('YTRR-22','Nissan','Skyline','2009','Petróleo','Manual','Negro','#A9283422','13.002','Sedan','2022-06-08 00:06:20','2022-06-08 00:06:20');
+/*!40000 ALTER TABLE `vehicle` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `workorder`
+--
+
+DROP TABLE IF EXISTS `workorder`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `workorder` (
-  `ID` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish2_ci NOT NULL
+  `ID` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish2_ci NOT NULL,
+  PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Volcado de datos para la tabla `workorder`
+-- Dumping data for table `workorder`
 --
 
-INSERT INTO `workorder` (`ID`) VALUES
-('1'),
-('2'),
-('3'),
-('4'),
-('5'),
-('6');
+LOCK TABLES `workorder` WRITE;
+/*!40000 ALTER TABLE `workorder` DISABLE KEYS */;
+INSERT INTO `workorder` VALUES ('1'),('2'),('3'),('4'),('5'),('6');
+/*!40000 ALTER TABLE `workorder` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `client`
---
-ALTER TABLE `client`
-  ADD PRIMARY KEY (`RUT`);
-
---
--- Indices de la tabla `mechanic`
---
-ALTER TABLE `mechanic`
-  ADD PRIMARY KEY (`RUT`);
-
---
--- Indices de la tabla `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`ID`);
-
---
--- Indices de la tabla `vehicle`
---
-ALTER TABLE `vehicle`
-  ADD PRIMARY KEY (`PATENT`);
-
---
--- Indices de la tabla `workorder`
---
-ALTER TABLE `workorder`
-  ADD PRIMARY KEY (`ID`);
-
---
--- Restricciones para tablas volcadas
---
-
---
--- Filtros para la tabla `user`
---
-ALTER TABLE `user`
-  ADD CONSTRAINT `IDWO` FOREIGN KEY (`ID`) REFERENCES `workorder` (`ID`);
-COMMIT;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-06-08  1:40:04
